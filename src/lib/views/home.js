08d8f99fd376fbf13c32f75eded825a7ae065fb5
Marianny Garcia <mdvgarciar@gmail.com>
2021-08-05 12:07:18 -0400

    cambios

--- src/lib/views/home.js
+++ src/lib/views/home.js
@@ -3,11 +3,18 @@ export default ()=>{
     <a href="#profile">profile</a>
     <section class="carousel">
     <ul>
-      <li><img src="imgMovie/anabelle.jpg" alt=""></li>
-      <li><img src="imgMovie/infinitywar.jpg" alt=""></li>
-      <li><img src="imgMovie/interestelar.jpg" alt=""></li>
-      <li><img src="imgMovie/joker.jpg" alt=""></li>
-      <li><img src="imgMovie/toystory4.jpg" alt=""></li>
+    <li><img src="data/imgPortada/ava.jpg" alt=""></li>
+    <li><img src="data/imgPortada/blackWidow.jpg" alt=""></li>
+    <li><img src="data/imgPortada/bloodRedSky.jpg" alt=""></li>
+    <li><img src="data/imgPortada/fearStreetPartThree.jpg" alt=""></li>
+    <li><img src="data/imgPortada/girlNext.jpg" alt=""></li>
+    <li><img src="data/imgPortada/howibecameasuperhero.jpg" alt=""></li>
+    <li><img src="data/imgPortada/jolt.png" alt=""></li>
+    <li><img src="data/imgPortada/laManoDelDemonio.jpg" alt=""></li>
+    <li><img src="data/imgPortada/oxygen.jpg" alt=""></li>
+    <li><img src="data/imgPortada/thepParamedic.jpg" alt=""></li>
+    <li><img src="data/imgPortada/thunderForce.jpeg" alt=""></li>
+    <li><img src="data/imgPortada/unidos.jpg" alt=""></li>
     </ul>
   </section>
     </div>`;
