fc00e7c526c0d6a6dc7e3d15b89f895f23d22df6
danielamendezgandara <daniela.a.mendezgandara@gmail.com>
2021-08-09 11:06:38 -0400

    Nuevos cambios en Components.js

--- src/lib/views/Components.js
+++ src/lib/views/Components.js
@@ -2,10 +2,10 @@ import Home from './Home.js';
 import Profile from './Profile.js';
 import Register from './Register.js';
 const components = {
-     '#': Register(),
-     '#home': Home(),
-     '#profile': Profile(),
-     '': Home()
+     "#": Register(),
+     "#home": Home(),
+     "#profile": Profile(),
+     "": Home()
 }
 
 export default components;
