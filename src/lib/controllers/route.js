a454568f927d8a391666353da3444b7b16c7618d
danielamendezgandara <daniela.a.mendezgandara@gmail.com>
2021-08-09 10:57:18 -0400

    Nuevos cambios

--- src/lib/controllers/route.js
+++ src/lib/controllers/route.js
@@ -4,6 +4,7 @@ const page=document.getElementById('root');
 
 const changeRoute=(route)=>{
     const hash=route.replace(/#/g,'');
+    console.log("changeRoute");
     window.history.replaceState({},hash,'/'+`${hash}`);
 }
 
@@ -14,11 +15,13 @@ export const changePath=(path)=>{
     /*if(user){
        window.location.hash=''
     }*/
+    console.log("changePath");
     page.appendChild(components[view]);
 
  }
 
 export const changeView=(route)=>{
+   console.log("changeView");
    page.innerHTML='';
    page.appendChild(components[route]);
    changeRoute(route);
