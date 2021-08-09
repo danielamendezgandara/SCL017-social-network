5eff889b9d4c37a0ed2cb592a81aee2139608c8c
danielamendezgandara <daniela.a.mendezgandara@gmail.com>
2021-08-09 16:09:12 -0400

    Modificación de main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import register from './lib/views/register.js';
 import {changePath, changeView } from './lib/controllers/route.js';
 
 window.addEventListener('hashchange',()=>changeView(window.location.hash));
-window.addEventListener('popstate',()=>changePath(window.location.pathname));
+//window.addEventListener('popstate',()=>changePath(window.location.pathname));
+if(window.location.pathname=='/home'){
+    
+}
 document.getElementById("root").appendChild(register());
 
