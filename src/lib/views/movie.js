7210ba9cc3196bcc0e1b27ce386bdfc41b9f953d
Marianny Garcia <mdvgarciar@gmail.com>
2021-08-10 17:14:09 -0400

    files

--- src/lib/views/movie.js
+++ src/lib/views/movie.js
@@ -1,11 +1,19 @@
-export const moviePost = () => {
+export const moviePost = (movieResult) => {
     window.location.hash='#movie';
-    console.log("hola");
+    const sheetContainer = document.createElement('div');
+    sheetContainer.id = "sheetContainerId";    
+    sheetContainer.innerHTML=  `<div class="nameContainer">
+    <p class="movieSheetName">#${movieResult.name}</p>
+    <div class="imgContainer">
+    <img class="movieImg" src="${movieResult.imgPortada}">
+    
+    
+    `;
+
+    
  };
 export default () =>{
-   const movieView= `<div> 
-   <button id="sign-out">Cerrar sesión</button>
-   </div>`;
+   const movieView= `<div id="movie"></div>`;
    const moviePage=document.createElement('div');
    moviePage.innerHTML=movieView;
    return moviePage;
