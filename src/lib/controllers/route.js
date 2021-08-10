a111637a343690e812ff15652d1a9e90566012eb
danielamendezgandara <daniela.a.mendezgandara@gmail.com>
2021-08-09 21:57:04 -0400

    Cambio en función changeRoute

--- src/lib/controllers/route.js
+++ src/lib/controllers/route.js
@@ -1,7 +1,5 @@
 
 export const changeRoute=(route)=>{
     const hash=route.replace(/#/g,'');
-    const hashpath='/'+hash;
-    console.log(hashpath);
-    return window.history.replaceState({},hash,hashpath);
+    return window.history.replaceState({},hash,'/'+`${hash}`);
 }
