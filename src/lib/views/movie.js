04bfe06c56fae3215c66c11a31d72f58651c626d
Marianny Garcia <mdvgarciar@gmail.com>
2021-08-10 14:40:40 -0400

    files

--- src/lib/views/movie.js
+++ src/lib/views/movie.js
@@ -1,9 +1,14 @@
-    
-const moviePost = () => {
-window.location.hash='#movie';
-console.log(movieName);
-document.body.innerHTML= movieName;
-
+export const moviePost = () => {
+    window.location.hash='#movie';
+    console.log("hola");
+ };
+export default () =>{
+   const movieView= `<div> 
+   <button id="sign-out">Cerrar sesión</button>
+   </div>`;
+   const moviePage=document.createElement('div');
+   moviePage.innerHTML=movieView;
+   return moviePage;
 }
 
 
@@ -14,9 +19,5 @@ document.body.innerHTML= movieName;
 
 
 
-
-
-
-export {moviePost};
 
 
