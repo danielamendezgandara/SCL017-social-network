7c80e96c428f9e1b92026f63499359afffc8605e
danielamendezgandara <daniela.a.mendezgandara@gmail.com>
2021-08-18 22:48:49 -0400

    Se restablecen funciones del registro y del login

--- src/lib/controllers/login-controller.js
+++ src/lib/controllers/login-controller.js
@@ -53,3 +53,70 @@ const signInUserGoogle = () =>{
   }
 
 export {signInUserEmailPassword,signInUserGoogle,signInUserFacebook};
+
+
+/*import { errorAuthUser, loginUserWithEmailPassword, signInUserFacebookAccount, signInUserGoogleAccount } from "../models/modelFirebase.js";
+
+const signInUserEmailPassword = () =>{
+    const email = document.getElementById("email").value;
+    const password=document.getElementById("password").value;
+    const message=document.getElementById("messageLogin");
+    loginUserWithEmailPassword(email,password).then((userLogin) => {
+        if(userLogin.user.emailVerified !== true){
+          message.innerHTML='Valida tu cuenta por favor. Revisa tu correo y regresa a iniciar sesión.';
+        }else{
+         window.location.hash='#wall';
+        }
+        })
+        .catch((error) => {
+        const errorCode = error.code;
+        message.innerHTML=errorAuthUser(errorCode);
+        });
+}
+
+const signInUserGoogle = () =>{
+  signInUserGoogleAccount().then((result) => {
+    const user=result.user;
+    const docRef=firebase.firestore().collection('users').doc(user.uid);
+    docRef.get().then((doc) => {
+      if (doc.exists) {
+        window.location.hash='#wall';
+      } else {
+         alert('Aún no se ha registrado.Por favor diríjase a la sección de registro con cuenta de Google.'); 
+      }
+  }).catch((error) => {
+      console.log("Error getting document:", error);
+  });
+
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    const credential = error.credential;
+    alert("Error detectado al usar la cuenta de Google");
+  });
+  }
+
+  const signInUserFacebook = () =>{
+    signInUserFacebookAccount().then((result) => {
+      window.location.hash='#wall';
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      const credential = error.credential;
+      console.log(errorCode);
+      if (errorCode === 'auth/account-exists-with-different-credential') {
+        alert('La dirección de correo electrónico ya está en uso.');
+      }
+    });
+  }
+
+export {signInUserEmailPassword,signInUserGoogle,signInUserFacebook};*/
--- src/lib/controllers/register-controller.js
+++ src/lib/controllers/register-controller.js
@@ -91,3 +91,114 @@ const signUpUserFacebook = () =>{
 }
 
 export {signUpUserEmailPassword, signUpUserGoogle,signUpUserFacebook};
+
+/*
+
+import {addCollection, authUserWithEmailPassword, errorAuthUser, sendEmailVerification, signInUserGoogleAccount, userActive } from "../models/modelFirebase.js";
+
+const signUpUserEmailPassword = () =>{
+    const email=document.getElementById("mail-creation").value;
+    const password=document.getElementById("pass-creation").value;
+    const userName=document.getElementById("user-creation").value;
+    const modalRegister=document.getElementById("myModal");
+    const form=document.getElementById("formAccCreation");
+    const message=document.getElementById("message");
+    authUserWithEmailPassword(email,password).then((newUser) => {
+        sendEmailVerification();
+        const objUser =  {
+            name : userName,
+            email: newUser.user.email,
+            photoURL:'https://img.icons8.com/ios-glyphs/120/000000/user-female.png',
+        };
+        addCollection('users',newUser.user.uid,objUser);
+        modalRegister.style.display='none';
+        alert('Revisa tu correo electrónico');
+        })
+       .catch((error) => {
+        const errorCode = error.code;
+        form.reset();
+        message.innerHTML= errorAuthUser(errorCode);
+        console.log(error);
+      });
+    }
+
+    const signUpUserGoogle = () =>{
+      signInUserGoogleAccount().then((result) => {
+        const user=result.user;
+        const focusEmail=document.getElementById("email");
+        const focusGoogle=document.getElementById("google");
+        //const usermail=result.user.email;
+        //const db=firebase.firestore();
+        const docRef=firebase.firestore().collection('users').doc(user.uid);
+        // console.log(result.user);
+        /*db.collection("users").where("email", "==",usermail)
+    .onSnapshot((querySnapshot) => {
+        var cities = [];
+        querySnapshot.forEach((doc) => {
+            cities.push(doc.data().name);
+        });
+        console.log("Current cities in CA: ", cities.join(", "));
+    });*/
+       /* docRef.get().then((doc) => {
+          if (doc.exists) {
+              alert("Esta cuenta ya se encuentra registrada ya sea mediante email y contraseña o a través de una cuenta de Google.Inicie sesión con alguna opción ofrecida.");
+              focusEmail.focus();
+              focusGoogle.focus();
+          } else {
+              // doc.data() will be undefined in this case
+              const objUser= {
+                name: user.displayName,
+                email: user.email,
+                photoURL: user.photoURL,
+                like_status: '',
+                notLike_status :'hide'
+              }
+              addCollection('users',user.uid,objUser);
+              console.log("No such document!");
+              window.location.hash='#wall';
+          }
+      }).catch((error) => {
+          console.log("Error getting document:", error);
+      });
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        const credential = error.credential;
+        alert("Error detectado al usar la cuenta de google");
+      });
+      }
+
+const signUpUserFacebook = () =>{
+    signInUserFacebookAccount().then((result) => {
+        const user = result.user;
+        const objUser= {
+              name: user.displayName,
+              email: user.email,
+              photoURL: user.photoURL,
+        };
+        addCollection('users',user.uid,objUser)
+        .then(() => {
+          window.location.hash = '#wall';
+        });
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        const credential = error.credential;
+        console.log(errorCode);
+        if (errorCode === 'auth/account-exists-with-different-credential') {
+          alert('La dirección de correo electrónico ya esta en uso');
+        }
+      });
+}*/
+
+
+//export {signUpUserEmailPassword,signUpUserGoogle,signUpUserFacebook};*/
