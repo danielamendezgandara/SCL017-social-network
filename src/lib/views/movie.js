2c9cd301e550a9b657f111f8f5b5b8f5f322f1c8
danielamendezgandara <daniela.a.mendezgandara@gmail.com>
2021-08-25 17:47:34 -0400

    Modificación de archivo movie.js : corrección de errores detectados por eslint

--- src/lib/views/movie.js
+++ src/lib/views/movie.js
@@ -1,34 +1,24 @@
-/*Se llama a la data del usuario específico para obtener el nombre de la película clickeada que está almacenada
-en el elemento movieView del objeto del usuario y se manda a viewMovie que tiene la estructura de la reseña de la
-película*/
+import { viewMovie } from './viewMovie.js';
 
-import { viewMovie } from "./viewMovie.js";
-
-export default ()=>{
-  const movieView = `<div class="sheetContainer"></div>`;
-  const moviePage = document.createElement("div");
+export default () => {
+  const movieView = '<div class="sheetContainer"></div>';
+  const moviePage = document.createElement('div');
   moviePage.innerHTML = movieView;
-  const sheetContainer=moviePage.querySelector('.sheetContainer');
+  const sheetContainer = moviePage.querySelector('.sheetContainer');
   firebase.auth().onAuthStateChanged((user) => {
-    if (user) { 
+    if (user) {
       const uid = user.uid;
-      const docUser=  firebase.firestore().collection('users').doc(uid);
-      docUser.get().then(async(doc) => {
+      const docUser = firebase.firestore().collection('users').doc(uid);
+      docUser.get().then(async (doc) => {
         if (doc.exists) {
-            console.log("Document data:", doc.data());
-            console.log(doc.data().movieView);
-            sheetContainer.appendChild(await viewMovie(doc.data().movieView));
+          sheetContainer.appendChild(await viewMovie(doc.data().movieView));
         } else {
-            // doc.data() will be undefined in this case
-            alert("Document no exist");
-        }
-    }).catch((error) => {
-        alert("Error getting document:", error);
-    });
-
+          alert('Document no exist');
+        } 
+      }).catch((error) => {
+        alert('Error getting document:', error);
+      });
     }
-});
-   
-return moviePage; 
-
- }
+  });
+  return moviePage;
+};
