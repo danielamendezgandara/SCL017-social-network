11490cb0824291709a5e822f20ee7cdf7542df4d
danielamendezgandara <daniela.a.mendezgandara@gmail.com>
2021-08-24 22:44:52 -0400

    Modificación de las funciones de profile.js : se corrige los errores detectados por eslint

--- src/lib/views/profile.js
+++ src/lib/views/profile.js
@@ -1,20 +1,19 @@
-import { signOutUser } from "../models/modelFirebase.js";
+import { signOutUser } from '../models/modelFirebase.js';
+
 export const eventSignOut = () => {
-   signOutUser()
-     .then(() => {
-       window.location.pathname = '/';
-     }).catch((error) => {
-     // An error happened.oto
-       console.log(error);
-     });
- };
-export default () =>{
-   const profileView= `<div> 
-   <button id="sign-out">Cerrar sesión</button>
-   </div>`;
-   const profilePage=document.createElement('article');
-   profilePage.innerHTML=profileView;
-   const signOut=profilePage.querySelector('#sign-out');
-   signOut.addEventListener('click',eventSignOut,false);
-   return profilePage;
-}
+  signOutUser()
+    .then(() => {
+      window.location.pathname = '/';
+    });
+};
+
+export default () => {
+  const profileView = `<div> 
+                    <button id="sign-out">Cerrar sesión</button>
+                    </div>`;
+  const profilePage = document.createElement('article');
+  profilePage.innerHTML = profileView;
+  const signOut = profilePage.querySelector('#sign-out');
+  signOut.addEventListener('click', eventSignOut, false);
+  return profilePage;
+};
